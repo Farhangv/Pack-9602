d6c408482b00c07257d83365d3c3e84f1d53cb1a
Farhangv <farhangv@live.com>
2017-07-29 13:22:42 +0430

    960507

---
+++ Session32-960507/DynamicDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicDemo
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            dynamic person = new { Name = "" };
+            person.Name = "John";
+            person.Family = "Doe";
+
+
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Family);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Session32-960507/MVCDemo/Controllers/StudentsController.cs
@@ -0,0 +1,32 @@
+﻿using MVCDemo.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public ActionResult Index()
+        {
+            var students = StudentRepository.GetAllStudents();
+            //ViewData["Students"] = students;
+            //ViewBag.Students = students;
+            return View(students);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(string name,string family)
+        {
+            //ViewBag.Message = "دانشجوی جدید با موفقیت ثبت شد";
+            TempData["Message"] = "دانشجوی جدید با موفقیت ثبت شد";
+            return RedirectToAction("Index");
+        }
+    }
+}

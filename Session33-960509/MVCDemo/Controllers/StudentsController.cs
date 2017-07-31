8cc138f31521631f138574809876f0e5a9f98c42
Farhangv <farhangv@live.com>
2017-07-31 13:20:54 +0430

    960509

---
+++ Session33-960509/MVCDemo/Controllers/StudentsController.cs
@@ -0,0 +1,52 @@
+﻿using MVCDemo.DbFirstModel;
+using MVCDemo.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public ActionResult Index()
+        {
+            var students = StudentRepository.GetAllStudents();
+            //ViewData["Students"] = students;
+            //ViewBag.Students = students;
+            return View(students);
+        }
+
+        //[HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //public ActionResult Create(FormCollection form)
+        //public ActionResult Create(string name, string family, int age)
+        public ActionResult Create(Student student)
+        {
+            //var name = Request.QueryString["Name"];
+
+            //var name = Request.Form["Name"];
+            //var family = Request.Form["Family"];
+
+            //var name = Request["Name"];
+            //var family = Request["Family"];
+
+            //var name = form["Name"];
+            //var family = form["Family"];
+            //var age = int.Parse(form["Age"]);
+
+            //StudentRepository.CreateStudent(name, family);
+            StudentRepository.CreateStudent(student);
+            TempData["Message"] = $"دانشجو {student.Name} {student.Family} با موفقیت افزوده شد.";
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ Session33-960509/MVCDemo/Repository/StudentRepository.cs
@@ -0,0 +1,37 @@
+﻿using MVCDemo.DbFirstModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Repository
+{
+    public class StudentRepository
+    {
+        public static IEnumerable<Student> GetAllStudents()
+        {
+            //return new List<Student>() {
+            //    new Student() { Id = 1, Name = "جان", Family = "دو" },
+            //    new Student() { Id = 2, Name = "علی", Family = "علیان" },
+            //    new Student() { Id = 3, Name = "سیاوش", Family = "نوری" },
+            //    new Student() { Id = 4, Name = "مهران", Family = "مهرانی" }
+            //};
+            StudentPortalEntities db = new StudentPortalEntities();
+            return db.Students.ToList();
+        }
+
+        public static bool CreateStudent(string name, string family)
+        {
+            return true;
+        }
+
+        public static bool CreateStudent(Student student)
+        {
+            StudentPortalEntities db = new StudentPortalEntities();
+            db.Students.Add(student);
+            db.SaveChanges();
+            return true;
+        }
+
+    }
+}
